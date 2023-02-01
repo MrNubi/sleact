7f60ee6a01d1cecf9c1806fb7d921cdac900cd95
jahun <99630193+MrNubi@users.noreply.github.com>
2023-02-02 04:45:32 +0900

    일단 커밋 이건 반영하기 힘들것다

---
+++ realcode/layouts/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from '@pages/Login';
+import SignUp from '../pages/SignUp';
+
+const App = () => {
+  return <div>초기 세팅입니다.</div>;
+};
+
+export default App;
